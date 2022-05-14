6f27e53fc7be12a8fdca144a0c64eb270a41429f
sirSayed98 <mohamed.sayed8899@gmail.com>
2022-05-14 22:00:46 +0200

    test: :test_tube: (BONUS #1)
    e2e testing

---
+++ cypress/integration/1-getting-started/second.scenario.spec.js
@@ -0,0 +1,16 @@
+/* eslint-disable quotes */
+/* eslint-disable no-undef */
+
+describe("second-scenario", () => {
+  it("fetch employees", () => {
+    cy.visit("http://localhost:8080/");
+
+    cy.request("https://fe-task.getsandbox.com/employees").then((response) => {
+      expect(response.status).to.eq(200);
+    });
+
+    cy.get(".c-button").within(() => {
+      return cy.contains("Load More").click();
+    });
+  });
+});
