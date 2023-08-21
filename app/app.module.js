7e9d9ab6806360ba05e70a584ddfb1d6c1206b4c
sirSayed98 <mohamed.sayed8899@gmail.com>
2023-08-21 13:21:01 +0300

    use setRootVueInstanceProps

--- app/app.module.js
+++ app/app.module.js
@@ -1,25 +1,26 @@
-import Vue from 'vue';
 import 'ngVue';
 import 'ngVue/build/plugins.js';
-import PerformancePageComponent from './pages/performance-page.vue';
-import NotFound from './pages/404-page.vue';
-import PerformanceChartComponent from './components/vue-components/performance-chart.vue';
+import Vue from 'vue';
 import filterComponent from './components/vue-components/filter.vue';
+import PerformanceChartComponent from './components/vue-components/performance-chart.vue';
+import NotFound from './pages/404-page.vue';
+import PerformancePageComponent from './pages/performance-page.vue';
 import store from './store/index';
 
-angular.module('appModule', [
+const app = angular.module('appModule', [
   'ui.router',
   'ngVue',
   'ngVue.plugins',
 ]);
 
-angular.module('appModule').directive('vPerformancePage', (createVueComponent) => {
-  return createVueComponent(Vue.component('performancePageComponent', {
+app.config(($ngVueProvider) => {
+  $ngVueProvider.setRootVueInstanceProps({
     store,
-    render(h) {
-      return h(PerformancePageComponent);
-    },
-  }));
+  });
+});
+
+angular.module('appModule').directive('vPerformancePage', (createVueComponent) => {
+  return createVueComponent(Vue.component('performancePageComponent', PerformancePageComponent));
 });
 
 angular.module('appModule').directive('vPerformanceChart', (createVueComponent) => {
--- app/components/vue-components/performance-chart.vue
+++ app/components/vue-components/performance-chart.vue
@@ -1,25 +1,22 @@
 <template>
   <div class="c-chart__container">
-    <v-chart ref="chart" :option="chartOptions" />
+  <h1>Performance chart</h1>
 
   </div>
 
 </template>
 
 <script>
-import moment from "moment";
+import { LineChart } from "echarts/charts";
 import { use } from "echarts/core";
 import { CanvasRenderer } from "echarts/renderers";
-import { LineChart } from "echarts/charts";
 
 
 import {
-  TitleComponent,
-  TooltipComponent,
-  GridComponent,
-  VisualMapComponent,
+GridComponent, TitleComponent,
+TooltipComponent, VisualMapComponent
 } from "echarts/components";
-import VChart from "vue-echarts";
+
 
 
 use([
@@ -36,124 +33,11 @@ export default {
   name: "PerformanceChartComponent",
 
 
-  components: {
-    VChart,
+
+
+  created(){
+    console.log(this);
   },
 
-  computed: {
-
-    chartData() {
-      return this.$store.state.chartDataDisplayed
-    },
-
-    initOptions() {
-      return {
-        width: "auto",
-        height: "300px",
-      };
-    },
-
-    chartOptions() {
-      return {
-        title: {
-          text: "Team Performance Index",
-          left: "center",
-        },
-        visualMap: {
-          top: 50,
-          right: 10,
-          pieces: [
-            {
-              gt: 0,
-              lte: 50,
-              color: "red"
-            },
-            {
-              gt: 50,
-              lte: 80,
-              color: "yellow"
-            },
-            {
-              gt: 80,
-              lte: 100,
-              color: "green"
-            },
-
-          ],
-          outOfRange: {
-            color: "#999"
-          }
-        },
-
-        tooltip: {
-          enabled: true,
-          trigger: 'axis',
-          transitionDuration: 0,
-          confine: false,
-          hideDelay: 0,
-          padding: 10,
-          backgroundColor: "#16253F",
-          textStyle: {
-            color: "white",
-          },
-          extraCssText: "text-align: center",
-
-        },
-        grid: {
-          left: "30px",
-          right: "12px",
-          bottom: "2px",
-          top: "6px",
-          containLabel: true,
-        },
-        xAxis: {
-          type: "category",
-          showGrid: false,
-          data: this.xAxisData,
-          axisLine: {
-            show: true,
-          },
-          axisTick: {
-            show: false,
-          },
-          axisLabel: {
-            fontSize: 11,
-          },
-        },
-        yAxis: {
-          axisLabel: { show: true },
-          axisTick: { show: true },
-          splitLine: { show: true },
-        },
-        series: [
-          {
-            data: this.yAxisData,
-            name: "Team Performance Index :",
-            type: "line",
-            symbol: "circle",
-            symbolSize: 2,
-            cursor: "default",
-            lineStyle: {
-              width: 2,
-            },
-          },
-        ],
-      };
-    },
-
-    xAxisData() {
-      return this.chartData.map((item) => this.formatDate(item.date_ms));
-    },
-
-    yAxisData() {
-      return this.chartData.map((item) => +`${item.performance * 100}`);
-    },
-  },
-
-  methods: {
-    formatDate(dateInMs) {
-      return moment(dateInMs).format("DD MMM YYYY");
-    },
-  },
 };
 </script>
